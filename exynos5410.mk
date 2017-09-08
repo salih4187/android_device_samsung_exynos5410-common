40f3840ae0f07e043c8e1850f54140a365d5b8c0
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-09-09 01:33:29 +0200

    exynos5410-common: nfc: Rename nfc_nci.bcm2079x from .msm8960 to .default
    Change-Id: I7a271fee2cb73abb4c17493c972317bbcd83fdcf
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- exynos5410.mk
+++ exynos5410.mk
@@ -134,7 +134,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libnfc-nci \
     libnfc_nci_jni \
-    nfc_nci.bcm2079x.universal5410 \
+    nfc_nci.bcm2079x.default \
     NfcNci \
     Tag \
     com.android.nfc_extras
