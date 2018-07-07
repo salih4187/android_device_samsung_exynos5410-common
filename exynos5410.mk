6720f37cd6386ad9f69400fb82ce0e1cdcd46449
Nolen Johnson <johnsonnolen@gmail.com>
2018-07-07 19:09:49 -0400

    exynos5410-common: Remove hwui properties
    * elektroschmock noted these weren't needed as of AOSP 8.0.
    
    Change-Id: I7c94005065da58bbdb2a1e5b6873dfd84a14159c

--- exynos5410.mk
+++ exynos5410.mk
@@ -226,8 +226,5 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # call dalvik heap config
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
-# call hwui memory config
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
-
 # call the common proprietary setup
 $(call inherit-product, vendor/samsung/exynos5410-common/exynos5410-common-vendor.mk)
