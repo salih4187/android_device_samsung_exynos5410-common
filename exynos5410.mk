3776ff8462b9b6e767aea152ff67b9e1924b2ad9
dkati <dkatikaridis@gmail.com>
2018-07-07 18:45:53 -0400

    exynos5410-common: Build HIDL NFC service
    Change-Id: I58f876ddc3d9c5b103e882e7a9c02d004cc97abe

--- exynos5410.mk
+++ exynos5410.mk
@@ -147,6 +147,7 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl-bcm \
+    android.hardware.nfc@1.0-service \
     libnfc-nci \
     libnfc_nci_jni \
     nfc_nci.bcm2079x.default \
