3d001223c7f686a4a3ceb293b18467d860ed6286
Ethan Chen <intervigil@gmail.com>
2018-09-24 23:54:32 -0700

    exynos5410-common: Drop custom display flags
    * TARGET_USES_OVERLAY and USE_OPENGL_RENDERER are largely deprecated
    * Don't set triple buffering, it introduces latency
    * Don't use custom VSYNC offsets
    
    Change-Id: If2892c83726c3558a96b28a8404f49982ebb9ba0

--- system_prop.mk
+++ system_prop.mk
@@ -22,7 +22,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.running_without_sync_framework = true \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers = 3 \
     ro.bq.gpu_to_cpu_unsupported=1 \
     debug.sf.enable_gl_backpressure=1 \
     debug.sf.latch_unsignaled=1 \
