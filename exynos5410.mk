9d3c8918ec31b3ae4a4e70fc272fcc7f22fbbc34
Salih Çağlar <salihcaglar4187@gmail.com>
2026-01-14 22:48:59 +0300

    exynos5410-common: Updates for new BSP

--- exynos5410.mk
+++ exynos5410.mk
@@ -113,10 +113,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.universal5410
 
-# Linker
-PRODUCT_PACKAGES += \
-    libsamsung_symbols
-
 # Media profile
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
