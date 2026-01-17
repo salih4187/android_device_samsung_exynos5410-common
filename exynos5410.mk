0a636430e57db4e364c8823ddd13e847a3b4c06f
Salih Çağlar <salihcaglar4187@gmail.com>
2026-01-17 21:32:58 +0300

    exynos5410-common: Build Graphics Allocator Service

--- exynos5410.mk
+++ exynos5410.mk
@@ -100,6 +100,7 @@ PRODUCT_PACKAGES += \
 # HW composer
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.composer@2.1-impl \
     hwcomposer.exynos5 \
