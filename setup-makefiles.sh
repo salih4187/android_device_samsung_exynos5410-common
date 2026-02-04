e5028ed8423c5641fea78a28cce34f5690dd3dbc
Salih Çağlar <salihcaglar4187@gmail.com>
2026-02-04 13:35:25 +0300

    exynos5410-common: Move remaining files to vendor

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -2,7 +2,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2018 The Lineage OS Project
->>>>>>> 2cfc04d8 (jf: Rebrand proprietary-files extract scripts)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
