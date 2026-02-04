66b165b0792cd2d384f5a05bb31492880172adf9
Salih Çağlar <salihcaglar4187@gmail.com>
2026-02-04 13:56:25 +0300

    exynos5410-common: Add folders and symlinks during build Cherry-picked from: https://github.com/LineageOS/android_device_samsung_jf-common/commit/3273ac4d2eeba1e0491e7dd74bcad51c269b4547

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,6 +78,10 @@ BLUE_LED_PATH := "/sys/class/leds/led_b/brightness"
 BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
 CHARGING_ENABLED_PATH := "/sys/class/power_supply/battery/batt_lp_charging"
 
+# Filesystem
+BOARD_ROOT_EXTRA_FOLDERS := efs
+TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
+
 # Filesystems
 BLOCK_BASED_OTA := false
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
---
+++ config.fs
@@ -0,0 +1,5 @@
+ [efs/]
+mode: 0771
+user: AID_RADIO
+group: AID_SYSTEM
+caps: 0
