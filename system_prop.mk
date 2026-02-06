e961e562fe86de855cbba6239d7124340efb64ab
Salih Çağlar <salihcaglar4187@gmail.com>
2026-02-07 00:03:39 +0300

    exynos5410-common: Disable SF Latch Buffer Signaled property

--- system_prop.mk
+++ system_prop.mk
@@ -15,6 +15,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.running_without_sync_framework = true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers = 3 \
     debug.sf.enable_gl_backpressure=1 \
+    debug.sf.latch_unsignaled=1 \
     ro.opengles.version=131072 \
     ro.sf.lcd_density=480 \
     ro.zygote.disable_gl_preload=1
