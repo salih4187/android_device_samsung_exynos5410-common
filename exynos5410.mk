d415c528e7ec1f291fcf2526838e1754bd8584b7
Salih Çağlar <salihcaglar4187@gmail.com>
2026-02-10 01:27:37 +0300

    exynos5410-common: Build DRM HAL

--- exynos5410.mk
+++ exynos5410.mk
@@ -84,6 +84,11 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl-legacy \
     camera.universal5410
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl.legacy \
