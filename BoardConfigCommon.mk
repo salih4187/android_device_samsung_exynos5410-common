59187d7344f79f089edfcd36cb2bf82df422c9e2
Salih Çağlar <salihcaglar4187@gmail.com>
2026-02-10 01:56:41 +0300

    exynos5410-common: Remove EGL Cache Sizes

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -106,16 +106,6 @@ BOARD_USE_BGRA_8888_FB := true
 BOARD_USE_SAMSUNG_CAMERAFORMAT_NV21 := true
 USE_OPENGL_RENDERER := true
 
-# Shader cache config options
-# Maximum size of the  GLES Shaders that can be cached for reuse.
-# Increase the size if shaders of size greater than 12KB are used.
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-
-# Maximum GLES shader cache size for each app to store the compiled shader
-# binaries. Decrease the size if RAM or Flash Storage size is a limitation
-# of the device.
-MAX_EGL_CACHE_SIZE := 2048*1024
-
 # IR
 IR_HAL_SUFFIX := universal5410
 
