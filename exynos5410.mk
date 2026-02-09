367e02f96e18958b4d25c96a587628d1da13e6ca
Salih Çağlar <salihcaglar4187@gmail.com>
2026-02-09 19:42:24 +0300

    exynos5410-common: Do not call hwc and memtrack HAL twice * Do not call hwcomposer.exynos5 and memtrack.exynos5 twice. * exynos5.mk will call it.

--- exynos5410.mk
+++ exynos5410.mk
@@ -103,11 +103,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.mapper@2.0-impl-2.1 \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    hwcomposer.exynos5 \
-    memtrack.exynos5
+    android.hardware.graphics.mapper@2.0-impl-2.1
 
 # IR
 PRODUCT_PACKAGES += \
@@ -137,6 +133,11 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(COMMON_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(COMMON_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
+
+# Memory Tracker HAL
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
 
 # Misc
 PRODUCT_PACKAGES += \
